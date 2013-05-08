b3aa34a4a1bd8fe701ef5298a5730fa035a5fbc0
Peter Eisentraut <peter@eisentraut.org>
2013-05-08 17:16:48 -0400

    Install packages for building extensions

--- manifests/default.pp
+++ manifests/default.pp
@@ -6,8 +6,13 @@ apt::source { 'pgdg':
   key => 'ACCC4CF8',
 }
 
-package { 'postgresql-9.2':
+package { ['postgresql-9.2', 'postgresql-server-dev-9.2']:
   require => Apt::Source['pgdg'],
 }
+
+package { 'build-essential': }
+package { 'curl': }
+package { 'git': }
+package { 'unzip': }
 
 class { 'my-hadoop': }
