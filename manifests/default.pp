cee2e95f0d346111da72d05e1df080152d2976c6
Peter Eisentraut <peter@eisentraut.org>
2013-05-20 14:41:54 -0400

    Install contrib package

--- manifests/default.pp
+++ manifests/default.pp
@@ -6,7 +6,7 @@ apt::source { 'pgdg':
   key => 'ACCC4CF8',
 }
 
-package { ['postgresql-9.2', 'postgresql-server-dev-9.2']:
+package { ['postgresql-9.2', 'postgresql-server-dev-9.2', 'postgresql-contrib-9.2']:
   require => Apt::Source['pgdg'],
 }
 
